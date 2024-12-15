1e6d0dec51789b8dc9be63600670485554c9a6bc
jeffrey-johan <jeffreyjohan974@gmail.com>
2024-12-15 12:40:45 -0700

    APY L2 A4

---
+++ main.py
@@ -0,0 +1,15 @@
+setx = {"green", "blue"}
+
+sety = {"blue", "yellow"}
+
+print("Original set elements:")
+
+print(setx)
+
+print(sety)
+
+print("\n union of two said sets:")
+
+setz = setx.union(sety)
+
+print(setz)
